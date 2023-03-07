b47a9340cb4ae348b06697a6bea7de7e726b85a3
Arishemm <arishem51@gmail.com>
2023-03-07 11:24:44 +0700

    fix: more data

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -4,22 +4,78 @@ export const Portfolio: PORTFOLIO = [
   {
     name: "About Me",
     id: 1,
-    style: {},
+    style: {
+      backgroundColor: "rgb(255, 238, 88)",
+      height: "18%",
+      width: "20%",
+      left: "5%",
+      top: "8%",
+    },
   },
   {
     name: "Experience",
     id: 2,
+    style: {
+      backgroundColor: "rgb(66, 165, 245)",
+      height: "28%",
+      width: "14%",
+      left: "42%",
+      top: "12%",
+    },
   },
   {
     name: "Work",
     id: 3,
+    style: {
+      backgroundColor: "rgb(239, 83, 80)",
+      height: "18%",
+      width: "16%",
+      left: "12%",
+      top: "36%",
+    },
   },
   {
     name: "Contact",
     id: 4,
+    style: {
+      backgroundColor: "rgb(102, 187, 106)",
+      height: "14%",
+      width: "12%",
+      left: "45%",
+      top: "58%",
+    },
   },
   {
     name: "Resume",
     id: 5,
+    style: {
+      backgroundColor: "rgb(255, 167, 38)",
+      height: "24%",
+      width: "24%",
+      left: "68%",
+      top: "8%",
+    },
+  },
+  {
+    name: "Social Media",
+    id: 6,
+    style: {
+      backgroundColor: "rgb(141, 110, 99)",
+      height: "26%",
+      width: "20%",
+      left: "72%",
+      top: "62%",
+    },
+  },
+  {
+    name: "Other",
+    id: 7,
+    style: {
+      backgroundColor: "rgb(250, 250, 250)",
+      height: "24%",
+      width: "24%",
+      left: "8%",
+      top: "64%",
+    },
   },
 ];
