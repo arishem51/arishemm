f58af82cccb22caff1cb1985561d46d486c38d5d
Arishemm <arishem51@gmail.com>
2023-03-07 11:04:42 +0700

    feat: constant & types

---
+++ src/constant/index.ts
@@ -0,0 +1,25 @@
+import { PORTFOLIO } from "../types";
+
+export const Portfolio: PORTFOLIO = [
+  {
+    name: "About Me",
+    id: 1,
+    style: {},
+  },
+  {
+    name: "Experience",
+    id: 2,
+  },
+  {
+    name: "Work",
+    id: 3,
+  },
+  {
+    name: "Contact",
+    id: 4,
+  },
+  {
+    name: "Resume",
+    id: 5,
+  },
+];
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+import React from "react";
+
+export type PORTFOLIO = {
+  id: number;
+  name: string;
+  style: React.CSSProperties;
+}[];
