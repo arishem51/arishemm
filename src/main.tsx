2a9a48b50f27ec7d90ec100dbd385ca2aa1ab37f
Arishemm <arishem51@gmail.com>
2023-03-07 11:04:28 +0700

    chore: App directory

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createGlobalStyle } from "styled-components";
-import App from "./App";
+import App from "./components/App";
 
 const GlobalStyles = createGlobalStyle`
   /*
