b0cde949e9439c7acb5d0fb58a2191806e375c60
Arishemm <arishem51@gmail.com>
2023-03-07 10:33:52 +0700

    chore: GlobalStyles components

--- src/main.tsx
+++ src/main.tsx
@@ -68,6 +68,7 @@ p, h1, h2, h3, h4, h5, h6 {
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
