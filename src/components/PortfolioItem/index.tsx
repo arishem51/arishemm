e62d7a4688ce7acfd3408f30646674739e927014
Arishemm <arishem51@gmail.com>
2023-03-07 11:13:05 +0700

    feat: PortfolioItem

---
+++ src/components/PortfolioItem/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 300px;
+  height: 400px;
+  border-radius: 1em;
+
+  position: absolute;
+`;
+
+type Props = {
+  className?: string;
+};
+
+export default function PortfolioItem(props: Props) {
+  return <Wrapper {...props}></Wrapper>;
+}
