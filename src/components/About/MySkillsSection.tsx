8b8940d2de370737787fdcc522002f4749509199
Arishemm <arishem51@gmail.com>
2023-03-11 12:19:05 +0700

    feat: MySkills

---
+++ src/components/About/MySkillsSection.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import AboutTitle from "./AboutTitle";
+
+const Wrapper = styled.section``;
+
+const MySkills = () => {
+  return (
+    <Wrapper>
+      <AboutTitle>My Skills</AboutTitle>
+    </Wrapper>
+  );
+};
+
+export default MySkills;
