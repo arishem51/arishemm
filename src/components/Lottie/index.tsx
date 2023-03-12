bcf6c48ff71ee0e859a70a70088f49d6ede5a815
Arishemm <arishem51@gmail.com>
2023-03-12 13:20:47 +0700

    feat: import lottie

--- src/components/Lottie/index.tsx
+++ src/components/Lottie/index.tsx
@@ -1,16 +1,14 @@
-import React from "react";
 import { Player, IPlayerProps } from "@lottiefiles/react-lottie-player";
+import { CSSProperties } from "styled-components";
 
 type Props = IPlayerProps;
 
+const styles: CSSProperties = {
+  position: "relative",
+  zIndex: 2,
+  height: "100%",
+};
+
 export default function Lottie(props: Props) {
-  return (
-    <Player
-      className="player"
-      loop
-      autoplay
-      style={{ position: "relative", zIndex: 2, height: "100%" }}
-      {...props}
-    />
-  );
+  return <Player className="player" loop autoplay style={styles} {...props} />;
 }
