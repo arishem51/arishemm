757e25d62c5ec79cb178ab065ee0092a9e504ed7
Arishemm <arishem51@gmail.com>
2023-03-13 08:25:41 +0700

    feat: useStack

--- src/components/ExpandedWrapper/index.tsx
+++ src/components/ExpandedWrapper/index.tsx
@@ -1,22 +1,23 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
+import Stack from "../Stack";
 
 type Props = {
   children: ReactNode;
 };
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8em;
-
+const Wrapper = styled(Stack)`
   width: 100%;
   padding: 8em 0;
   padding-bottom: 10em;
 `;
 
 const ExpandedWrapper = ({ children }: Props) => {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper direction="column" gap="8em">
+      {children}
+    </Wrapper>
+  );
 };
 
 export default ExpandedWrapper;
