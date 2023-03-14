43a4903d05d6acc29b80525b3b9293a7838e278e
Arishemm <arishem51@gmail.com>
2023-03-14 19:17:00 +0700

    feat: createAnimationDefination

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,16 @@
+import { TargetAndTransition, Transition } from "framer-motion";
+
 export const timeOut = (time: number) => {
   return new Promise((res) => setTimeout(() => res(time), time));
+};
+
+export const createAnimationDefination = (
+  custom: number,
+  translateY: string,
+  transition: Transition
+): TargetAndTransition => {
+  return {
+    translateY: translateY,
+    transition: { ...transition, delay: custom * 0.1 },
+  };
 };
