e919ac1d00a5b3f25bcd578ad563b71b7bd40a48
Arishemm <arishem51@gmail.com>
2023-03-14 19:16:53 +0700

    feat: update App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
-import { motion } from "framer-motion";
+import { motion, useAnimationControls, Variants } from "framer-motion";
 import styled from "styled-components";
 import Portfolio from "./Portfolio";
 import { useAnimationRefProvider } from "../Provider/AnimationProvider";
 import Heading from "./Heading";
 import Navbar from "./Navbar";
+import { useCallback } from "react";
+import Onboard from "./Onboard";
 
 const Wrapper = styled(motion.main)`
   position: relative;
@@ -11,18 +13,39 @@ const Wrapper = styled(motion.main)`
   width: 100vw;
   height: 100vh;
 
+  z-index: 1;
+
   overflow: hidden;
 `;
 
+const variants: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
+
 function App() {
+  const controls = useAnimationControls();
   const { viewRef } = useAnimationRefProvider();
 
+  const handleAnimation = useCallback(() => {
+    controls.start("visible");
+  }, [controls]);
+
   return (
-    <Wrapper ref={viewRef}>
-      <Portfolio />
-      <Heading>Arishemm</Heading>
-      <Navbar />
-    </Wrapper>
+    <>
+      <Onboard onOnboardUnmount={handleAnimation} />
+      <motion.div animate={controls} initial="initial" variants={variants}>
+        <Wrapper ref={viewRef}>
+          <Portfolio />
+          <Heading>Arishemm</Heading>
+          <Navbar />
+        </Wrapper>
+      </motion.div>
+    </>
   );
 }
 
