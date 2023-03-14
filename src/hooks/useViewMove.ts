44c8b0beeb3aa46972f32f24919af0ab59ac498c
Arishemm <arishem51@gmail.com>
2023-03-14 07:36:39 +0700

    feat: update useViewMove

--- src/hooks/useViewMove.ts
+++ src/hooks/useViewMove.ts
@@ -4,7 +4,7 @@ import {
   useMotionValue,
   useSpring,
 } from "framer-motion";
-import React from "react";
+import React, { useCallback } from "react";
 import { useRef } from "react";
 
 const OPPOSITE = -1;
@@ -13,12 +13,17 @@ const config: SpringOptions = { damping: 30, stiffness: 100 };
 
 type Dimensions = { width: number; height: number };
 
+let reverseViewX = 0;
+let reverseViewY = 0;
+
+const state: Dimensions = {
+  width: 0,
+  height: 0,
+};
+
 export function useViewMove() {
-  const [view, setView] = React.useState<Dimensions>({ width: 0, height: 0 });
-  const [content, setContent] = React.useState<Dimensions>({
-    width: 0,
-    height: 0,
-  });
+  const [view, setView] = React.useState<Dimensions>(state);
+  const [content, setContent] = React.useState<Dimensions>(state);
 
   React.useEffect(() => {
     setView({
@@ -41,39 +46,47 @@ export function useViewMove() {
   const springX: MotionValue<number> = useSpring(motionX, config);
   const springY: MotionValue<number> = useSpring(motionY, config);
 
-  function handleMouseMoveOnView(e: MouseEvent) {
-    const { clientX, clientY } = e;
+  const handleMouseMoveOnView = useCallback(
+    (e: MouseEvent) => {
+      const { clientX, clientY } = e;
 
-    if (
-      view.height === 0 ||
-      view.width === 0 ||
-      content.width === 0 ||
-      content.height === 0
-    ) {
-      return;
-    }
+      if (
+        view.height === 0 ||
+        view.width === 0 ||
+        content.width === 0 ||
+        content.height === 0
+      ) {
+        return;
+      }
 
-    const percentageX = clientX / view.width;
-    const percentageY = clientY / view.height;
+      const percentageX = clientX / view.width;
+      const percentageY = clientY / view.height;
 
-    const maxX = content.width - view.width;
-    const maxY = content.height - view.height;
+      const maxX = content.width - view.width;
+      const maxY = content.height - view.height;
 
-    const distanceX = percentageX * maxX * OPPOSITE;
-    const distanceY = percentageY * maxY * OPPOSITE;
+      const distanceX = percentageX * maxX * OPPOSITE;
+      const distanceY = percentageY * maxY * OPPOSITE;
 
-    motionX.set(distanceX);
-    motionY.set(distanceY);
-  }
+      motionX.set(distanceX);
+      motionY.set(distanceY);
+    },
+    [content.height, content.width, motionX, motionY, view.height, view.width]
+  );
 
-  const removeViewMoveEvent = () =>
-    viewRef.current?.removeEventListener("mousemove", handleMouseMoveOnView);
+  const removeViewMoveEvent = useCallback(
+    () =>
+      viewRef.current?.removeEventListener("mousemove", handleMouseMoveOnView),
+    [handleMouseMoveOnView]
+  );
 
-  const addViewMoveEvent = () =>
-    viewRef.current?.addEventListener("mousemove", handleMouseMoveOnView);
+  const addViewMoveEvent = useCallback(
+    () => viewRef.current?.addEventListener("mousemove", handleMouseMoveOnView),
+    [handleMouseMoveOnView]
+  );
 
-  const reverseViewX = motionX.get() * OPPOSITE;
-  const reverseViewY = motionY.get() * OPPOSITE;
+  reverseViewX = motionX.get() * OPPOSITE;
+  reverseViewY = motionY.get() * OPPOSITE;
 
   return {
     viewRef,
