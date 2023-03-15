c2536ed8d6f3fdb8a4283f582e054ef572f5707c
Arishemm <arishem51@gmail.com>
2023-03-15 23:07:27 +0700

    feat: useLottie

--- src/components/Lottie/index.tsx
+++ src/components/Lottie/index.tsx
@@ -1,8 +1,6 @@
 import { Player, IPlayerProps } from "@lottiefiles/react-lottie-player";
-import { AnimationItem } from "lottie-web";
-import { useCallback, useEffect, useRef, useState } from "react";
 import { CSSProperties } from "styled-components";
-import { usePortfolioProvider } from "../../Provider/AnimationProvider";
+import { useLottie } from "../../hooks/useLottie";
 
 type Props = IPlayerProps & {
   defaultFrame: number;
@@ -17,38 +15,10 @@ const styles: CSSProperties = {
   alignItems: "center",
 };
 
-const TIME = 750 / 1000; // seconds
+const TIME = 650 / 1000; // seconds
 
 export default function Lottie({ defaultFrame, ...props }: Props) {
-  const [lottie, setLottile] = useState<AnimationItem | undefined>(undefined);
-
-  const { portfolio } = usePortfolioProvider();
-
-  useEffect(() => {
-    if (lottie && !portfolio) {
-      const frame =
-        lottie.currentFrame !== 0 ? lottie.currentFrame : defaultFrame;
-      lottie.goToAndStop(frame, true, lottie.name);
-    }
-  }, [defaultFrame, lottie, portfolio]);
-
-  useEffect(() => {
-    if (!portfolio || !lottie) {
-      return;
-    }
-    lottie.play();
-    const removeListerner = lottie?.addEventListener("complete", () => {
-      lottie?.goToAndPlay(0, true);
-    });
-    return () => {
-      lottie?.goToAndStop(lottie.currentFrame, true);
-      removeListerner();
-    };
-  }, [lottie, portfolio]);
-
-  const setLottileInstance = useCallback((lottie: AnimationItem) => {
-    setLottile(lottie);
-  }, []);
+  const { setLottileInstance } = useLottie({ defaultFrame });
 
   return (
     <Player
