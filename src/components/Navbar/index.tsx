55140240d084eb68187314a9e297ec00825005e3
Arishemm <arishem51@gmail.com>
2023-03-17 20:12:01 +0700

    feat: optimize Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,9 @@
-import { LayoutGroup } from "framer-motion";
+import {
+  LayoutGroup,
+  motion,
+  useAnimationControls,
+  Variants,
+} from "framer-motion";
 import styled from "styled-components";
 import {
   useAnimationAPIProvider,
@@ -8,8 +13,9 @@ import {
 import { PortfolioType } from "../../types";
 import ListItem from "./ListItem";
 import Stack from "../Stack";
+import { useEffect } from "react";
 
-const Wrapper = styled.nav`
+const Wrapper = styled(motion.nav)`
   position: fixed;
   bottom: 1em;
   left: 50%;
@@ -32,11 +38,33 @@ const Menu: PortfolioType[] = [
   "resume",
 ];
 
+const variants: Variants = {
+  initial: {
+    opacity: 0,
+    zIndex: -1,
+  },
+  visible: {
+    opacity: 1,
+    zIndex: 2,
+  },
+};
+
 const Navbar = () => {
   const { setPortfolio, setAnimationType, setIsAnimationSlideUpRunning } =
     useAnimationAPIProvider();
   const { portfolio } = usePortfolioProvider();
   const { isAnimationSlideUpRunning } = useAnimationSlideUpProvider();
+  const controls = useAnimationControls();
+
+  useEffect(() => {
+    if (portfolio) {
+      controls.start("visible", {
+        delay: 0.75,
+      });
+    } else {
+      controls.start("initial");
+    }
+  }, [controls, portfolio]);
 
   function handleItemClick(item: PortfolioType) {
     if (isAnimationSlideUpRunning || portfolio === item) {
@@ -47,7 +75,7 @@ const Navbar = () => {
     setAnimationType("slideUp");
   }
 
-  function renderItem() {
+  const renderItem = () => {
     return Menu.map((item, index) => {
       return (
         <ListItem
@@ -61,14 +89,10 @@ const Navbar = () => {
         </ListItem>
       );
     });
-  }
-
-  if (!portfolio) {
-    return null;
-  }
+  };
 
   return (
-    <Wrapper>
+    <Wrapper animate={controls} variants={variants} initial="initial">
       <LayoutGroup>
         <List htmlElement="ul">{renderItem()}</List>
       </LayoutGroup>
